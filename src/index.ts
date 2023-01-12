6cc02aab1dc7717b41e2d62b390d2fe2ca13fdbd
Dany Fedorov <fedorov.dda@gmail.com>
2023-01-13 00:02:20 +0200

    fix: Add exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './PojoConstructor/PojoConstructor';
+export * from './PojoConstructor/constructPojo';
+export * from './PojoConstructorAsync/PojoConstructorAsync';
+export * from './PojoConstructorAsync/constructPojoAsync';
+export * from './PojoConstructorSync/PojoConstructorSync';
+export * from './PojoConstructorSync/constructPojoSync';
