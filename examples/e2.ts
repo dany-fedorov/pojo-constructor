7946d963b2740d7f7562a8b4db9565ef355b3fd5
Dany Fedorov <fedorov.dda@gmail.com>
2023-01-15 05:29:37 +0200

    chore: Remove unused examples

--- examples/e2.ts
+++
@@ -1,9 +0,0 @@
-import { PojoConstructorSync } from '../src/PojoConstructorSync/PojoConstructorSync';
-
-const ctor = new PojoConstructorSync<{ a: 123 }>({
-  a() {
-    return { value: 123 };
-  },
-});
-
-console.log(ctor.new());
